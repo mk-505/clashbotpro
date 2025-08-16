e7ff6d07f5f50b9cc08ddd163edc73824b0c7283
Shawn Xu <x57xu@uwaterloo.ca>
2025-08-16 16:20:28 -0400

    updates useful scripts

--- cut_episode_script_single.bash
+++
@@ -1,4 +0,0 @@
-python katacr/build_train_dataset/cut_episodes.py --path-video "/home/yy/Coding/datasets/CR/fast_pig_2.6/OYASSU_20230704.mp4"
-python katacr/build_train_dataset/cut_episodes.py --path-video "/home/yy/Coding/datasets/CR/fast_pig_2.6/OYASSU_20230729.mp4"
-python katacr/build_train_dataset/cut_episodes.py --path-video "/home/yy/Coding/datasets/CR/fast_pig_2.6/OYASSU_20230910.mp4"
-
